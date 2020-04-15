6d31a9968280be2e6aff8b755b3e65120061ab7c
Francesco Murdaca <fmurdaca@redhat.com>
2020-04-15 20:52:35 +0200

    Add integration tests for solved python packages
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

---
+++ features/package_versions_solved.feature
@@ -0,0 +1,10 @@
+Feature: Querying Thoth for Python package versions solved for all solvers
+
+    Scenario: Querying for Python package versions solved
+        Given a list of packages
+            | package_name |
+            | setuptools   |
+        And number of versions for each package_name is available
+        And deployment is accessible using HTTPS
+        When I query for the solved packages for the package_name from PyPI in Thoth Knowledge Graph
+        Then I should get the same number provided from PyPI
---
+++ features/steps/python_package_solved.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# Thoth's integration tests
+# Copyright(C) 2019 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Python Package solved integration tests for Thoth deployment."""
+
+import os
+import requests
+import urllib.parse
+
+from behave import given, when, then
+from hamcrest import assert_that, equal_to
+
+
+@given("a list of packages")
+def step_impl(context):
+    context.result = {}
+    for row in context.table:
+        context.result[row["package_name"]] = {}
+
+
+@given("number of versions for each package_name is available")
+def step_impl(context):
+    # TODO: check number of solvers
+    thoth_solvers = 3
+    for package in context.result.keys():
+        url = "https://pypi.python.org/pypi/{}/json".format(package)
+        data = requests.get(url).json()
+        number_pypi = len(list(data["releases"].keys()))
+        context.result[package]["number_pypi"] = number_pypi*thoth_solvers
+
+
+@when("I query for the solved packages for the package_name from PyPI in Thoth Knowledge Graph")
+def step_impl(context):
+    for package in context.result.keys():
+        response = requests.get(f"{context.scheme}://{context.api_url}/api/v1/python/packages/count?index={None}&name={package}&version={None}")
+        context.result[package]["number_thoth"] = response.json()["count"]
+
+
+@then("I should get the same number provided from PyPI")
+def step_impl(context):
+    for package in context.result.keys():
+        assert_that(context.result[package]["number_thoth"], equal_to(context.result[package]["number_pypi"]))
