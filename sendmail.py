ed2ace285eea25e66d081d565477c2da31dab003
Fridolin Pokorny <fridolin.pokorny@gmail.com>
2021-02-12 09:33:55 +0100

    Configure sending tests

---
+++ sendmail.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# Thoth's integration tests
+# Copyright(C) 2019 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/
+
+"""Send e-mail report, used to send reports to our mailing list."""
+
+from datetime import date
+from email.mime.text import MIMEText
+import os
+import smtplib
+
+_BEHAVE_REPORT_FILE = "behave-report.html"
+_DEPLOYMENT_NAME = os.getenv("THOTH_DEPLOYMENT_NAME", "N/A")
+_EMAIL_SMTP_SERVER = os.getenv("THOTH_EMAIL_SMTP_SERVER", "smtp.corp.redhat.com")
+_EMAIL_TO = os.getenv("THOTH_EMAIL_TO", "fpokorny@redhat.com")
+_EMAIL_FROM = os.getenv("THOTH_EMAIL_FROM", "noreply@redhat.com")
+
+
+def _create_email_subject() -> str:
+    """Create e-mail subject."""
+    today = date.today()
+    return f"Integration tests update for {_DEPLOYMENT_NAME} ({today.strftime('%Y-%m-%d')})"
+
+
+def send_email() -> None:
+    """Send e-mail with begave test reports."""
+    with open(_BEHAVE_REPORT_FILE, 'r') as fp:
+        msg = MIMEText(fp.read(), "html")
+
+    msg['Subject'] = _create_email_subject()
+    msg['From'] = _EMAIL_FROM
+    msg['To'] = _EMAIL_TO
+
+    s = smtplib.SMTP(_EMAIL_SMTP_SERVER)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string())
+    s.quit()
+
+
+__name__ == "__main__" and send_email()
--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@
 export THOTH_USER_API_HOST=${THOTH_USER_API_HOST:-test.thoth-station.ninja}
 export THOTH_MANAGEMENT_API_HOST=${THOTH_MANAGEMENT_API_HOST:-management.test.thoth-station.ninja}
 export THOTH_AMUN_API_HOST=${THOTH_AMUN_API_HOST:-amun-api-thoth-test-core.apps.ocp.prod.psi.redhat.com}
+MAIL_REPORT=${MAIL_REPORT:-0}
+NO_INSTALL=${NO_INSTALL:-0}
 
 echo -e "------------------------------------------------------------------\n\n"
 echo "> Tests are executed against User API at $THOTH_USER_API_HOST"
@@ -13,5 +15,12 @@ echo -e "\n\n------------------------------------------------------------------\
 export PIPENV_HIDE_EMOJIS=1
 export PIPENV_COLORBLIND=1
 
-[[ $NO_INSTALL -eq "1" ]] || pipenv install --deploy
-pipenv --verbose run behave --tags=~@local-test-only --show-timings $@
+[[ "${NO_INSTALL}" -eq "1" ]] || pipenv install --deploy
+
+if [ "${MAIL_REPORT}" -eq "1" ]; then
+    pipenv --verbose run behave --show-timings -f html -o behave-report.html $@
+    echo "Sending generated report..."
+    pipenv run python3 sendmail.py
+else
+    pipenv --verbose run behave --show-timings $@
+fi
