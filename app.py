81119691bbb1356559226ee9de4e4a256a707291
Kebechet <noreply+kebechet@redhat.com>
2022-03-08 18:14:55 +0000

    Release of version 0.10.0

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ import smtplib
 import sys
 import shutil
 
-__version__ = "0.9.3"
+__version__ = "0.10.0"
 
 _BEHAVE_REPORT_FILE = "behave-report.html"
 _DEPLOYMENT_NAME = os.getenv("THOTH_DEPLOYMENT_NAME", "N/A")
